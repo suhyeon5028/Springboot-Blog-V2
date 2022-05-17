bc1bfc17f16cf28ef8a6491a8ea06b7428ee529f
suhyeon <suhyeon5028@naver.com>
2022-05-17 20:09:46 +0900

    8. 쿠키로 유저네임 기억하기

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -9,6 +9,14 @@ $("#btn-login").click(() => {
 
 // 2. 기능
 
+// 유저네임 기억하기 메서드 httpOnly 속성이 걸려있으면 안된다 주의하자!!
+function usernameRemember() {
+  let cookies = document.cookie.split("=");
+  //console.log(cookies[1]);
+  $("#username").val(cookies[1]);
+}
+usernameRemember();
+
 // 회원가입 요청 메서드
 async function join() {
   // (1) username, password, email, addr 을 찾아서 오브젝트로 만든다.
@@ -20,7 +28,7 @@ async function join() {
   };
 
   // (2) fetch 요청한다. (json으로 변환해서)
-  let response = await fetch("/api/join", {
+  let response = await fetch("/join", {
     method: "POST",
     body: JSON.stringify(joinDto),
     headers: {
@@ -41,12 +49,16 @@ async function join() {
 
 // 로그인 요청 메서드
 async function login() {
+  // checkbox의 체크여부를 제이쿼리에서 확인하는 법
+  let checked = $("#remember").is(":checked");
+
   let loginDto = {
     username: $("#username").val(),
     password: $("#password").val(),
+    remember: checked ? "on" : "off",
   };
 
-  let response = await fetch("/api/login", {
+  let response = await fetch("/login", {
     method: "POST",
     body: JSON.stringify(loginDto),
     headers: {
