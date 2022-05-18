9387f0ad1044144edc76887af028a47e78583bd0
suhyeon <suhyeon5028@naver.com>
2022-05-18 21:22:15 +0900

    11. 회원정보 수정 완료 - 더티체킹

--- src/main/java/site/metacoding/blogv2/domain/user/UserRepository.java
+++ src/main/java/site/metacoding/blogv2/domain/user/UserRepository.java
@@ -7,5 +7,11 @@ import org.springframework.data.repository.query.Param;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query(value = "SELECT * FROM user WHERE username = :username AND password = :password", nativeQuery = true)
-    User mLogin(@Param("username") String username, @Param("password") String password);    
+    User mLogin(@Param("username") String username, @Param("password") String password);
+
+    // @Modifying
+    // @Query(value = "UPDATE user SET password = :password, email = :email, addr =
+    // :addr WHERE id = :id", nativeQuery = true)
+    // void mUpdate(@Param("password") String password, @Param("email") String
+    // email, @Param("addr") String addr, @Param("id") Integer id);
 }
