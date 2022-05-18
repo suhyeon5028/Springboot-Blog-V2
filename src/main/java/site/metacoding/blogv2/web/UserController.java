1ead32d0eee75ca7f837d7848ff52795f1b2be94
suhyeon <suhyeon5028@naver.com>
2022-05-18 21:56:43 +0900

    [회원정보 보기 CSR로 변경하기]

--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -3,12 +3,10 @@ package site.metacoding.blogv2.web;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
-import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 
 @RequiredArgsConstructor
@@ -20,9 +18,9 @@ public class UserController {
     // 앱은 이 친구 요청 할까? 아니!!
     // 웹은 이 친구 요청해야함.
     @GetMapping("/s/user/{id}")
-    public String userInfo(@PathVariable Integer id, Model model) {
-        User userEntity = userService.회원정보(id);
-        model.addAttribute("user", userEntity);
+    public String userInfo(@PathVariable Integer id /* , Model model */) {
+        // User userEntity = userService.회원정보(id);
+        // model.addAttribute("user", userEntity);
         return "user/updateForm";
     }
 
